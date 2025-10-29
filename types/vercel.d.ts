21b47ccde6198e6452e65827a2d02206937d307a
Adrin <adrinalias@gmail.com>
2025-10-29 00:45:41 -0500

    merge(pdf-portfolio)

---
+++ types/vercel.d.ts
@@ -0,0 +1,7 @@
+declare module '@vercel/analytics/react' {
+  export const Analytics: React.ComponentType;
+}
+
+declare module '@vercel/speed-insights/next' {
+  export const SpeedInsights: React.ComponentType;
+}
